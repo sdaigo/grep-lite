4a29886dd2b55614b1a0a017cbfe61d806bc0586
Daigo Shitara <stain.witness+github@gmail.com>
2021-06-26 15:45:35 +0900

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,55 @@
-fn main() {
-    let search_term = "picture";
+use clap::{App, Arg};
+use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books.
-What do we seek through millions of pages?";
-
-    for line in quote.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    let mut count = 0;
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => {
+                println!("{}", line);
+                count += 1;
+            }
+            None => (),
         }
+    }
+
+    println!("{} patterns found", count);
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
